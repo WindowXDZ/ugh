41282fbe890122ec02083141f24f791764f536ed
NotDarkzXD <180806148+WindowXDZ@users.noreply.github.com>
2025-08-14 17:34:57 -0300

    Update cmd.cmd

--- cmd.cmd
+++ cmd.cmd
@@ -6,11 +6,11 @@ set "PYFILE=%~dp0keepalive.py"
 rem Overwrite first line
 echo import time > "%PYFILE%"
 
-rem Append the infinite loop
+rem Append the infinite loop safely
 echo while True: >> "%PYFILE%"
 echo     time.sleep(3600) >> "%PYFILE%"
 
-rem Try to run Python
+rem Run Python if installed
 where py >nul 2>nul && py -u "%PYFILE%" && goto :eof
 where python >nul 2>nul && python -u "%PYFILE%" && goto :eof
 

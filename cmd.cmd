bf0b71d07067560755a58e81ae4a117ee27290d2
NotDarkzXD <180806148+WindowXDZ@users.noreply.github.com>
2025-08-14 17:25:36 -0300

    Update cmd.cmd

--- cmd.cmd
+++ cmd.cmd
@@ -1,3 +1,11 @@
 net user installer P0w!E4sy1nst4ll3r@
 net user runneradmin P0w!E4sy1nst4ll3r@
+@echo off
+cls
+echo DO NOT CLOSE THIS
+:loop
+echo work
+echo DO NOT CLOSE THIS
+timeout /t 5 >nul
+goto loop
 exit
